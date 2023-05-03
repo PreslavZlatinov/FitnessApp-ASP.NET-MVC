15e89d3fb25fd4958056ce393a642dd433a3d471
PreslavZlatinov <preslav.zlatinov@gmail.com>
2023-05-03 12:32:57 +0300

    Add project files.

---
+++ FitnessApp_PreslavZlatinov_19621621/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FitnessApp_PreslavZlatinov_19621621.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
