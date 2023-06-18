db625b1294e6bf5970de670ceb655a65b4d68a07
PreslavZlatinov <preslav.zlatinov@gmail.com>
2023-06-18 20:03:51 +0300

    Add Newsletter View and Controller

---
+++ FitnessApp_PreslavZlatinov_19621621/Controllers/NewsletterController.cs
@@ -0,0 +1,25 @@
+﻿using FitnessApp_PreslavZlatinov_19621621.Data.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessApp_PreslavZlatinov_19621621.Controllers
+{
+    public class NewsletterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SubscriptionCompleted()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Subscribe()
+        {
+            return View("SubscriptionCompleted");
+        }
+    }
+}
